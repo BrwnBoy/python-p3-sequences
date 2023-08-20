2767d8a28b470645dfa02dee4da62e735df4f7f6
Brwni <brianmm665@gmial.com>
2023-08-20 19:40:31 +0300

    Done with assignment

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length <= 0:
+        return []
+    elif length == 1:
+        return [0]
+    elif length == 2:
+        return [0, 1]
+    else:
+        fibonacci = [0, 1]
+        while len(fibonacci) < length:
+            fibonacci.append(fibonacci[-1] + fibonacci[-2])
+        return fibonacci
+pass
